f2a0c8d5f244f5a78ab885e602ebcb90b694ca61
xing cong <121950263@qq.com>
2021-07-24 04:12:27 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,14 +17,16 @@ export default {
     HelloWorld
   },
   created(){
-
+    this.ok()
+  },
+  methods: {
+     ok(){
 
 
 
 fetch('https://gw.alipayobjects.com/os/antvdemo/assets/data/algorithm-category.json')
   .then((res) => res.json())
   .then((data) => {
-
     const container = document.getElementById('container');
     const width = container.scrollWidth;
     const height = container.scrollHeight || 500;
@@ -32,15 +34,55 @@ fetch('https://gw.alipayobjects.com/os/antvdemo/assets/data/algorithm-category.j
       container: 'container',
       width,
       height,
-     
+      linkCenter: true,
+      modes: {
+        default: [
+          {
+            type: 'collapse-expand',
+            onChange: function onChange(item, collapsed) {
+              const data = item.get('model');
+              data.collapsed = collapsed;
+              return true;
+            },
+          },
+          'drag-canvas',
+          'zoom-canvas',
+        ],
+      },
+      defaultNode: {
+        size: 26,
+        anchorPoints: [
+          [0, 0.5],
+          [1, 0.5],
+        ],
+      },
+      defaultEdge: {
+        type: 'cubic-vertical',
+      },
+      layout: {
+        type: 'dendrogram',
+        direction: 'TB', // H / V / LR / RL / TB / BT
+        nodeSep: 40,
+        rankSep: 100,
+      },
     });
 
     graph.node(function (node) {
+      let position = 'right';
+      let rotate = 0;
+      if (!node.children) {
+        position = 'bottom';
+        rotate = Math.PI / 2;
+      }
       return {
         label: node.id,
         labelCfg: {
-          position: node.children && node.children.length > 0 ? 'left' : 'right',
+          position,
           offset: 5,
+          style: {
+            rotate,
+            textAlign: 'start',
+          },
         },
       };
     });
@@ -49,14 +91,21 @@ fetch('https://gw.alipayobjects.com/os/antvdemo/assets/data/algorithm-category.j
     graph.render();
     graph.fitView();
 
-    if (typeof window !== 'undefined')
-      window.onresize = () => {
-        if (!graph || graph.get('destroyed')) return;
-        if (!container || !container.scrollWidth || !container.scrollHeight) return;
-        graph.changeSize(container.scrollWidth, container.scrollHeight);
-      };
+    
+     
   });
 
+
+
+      console.log(G6)
+    }
   }
+
+
 }
+
+
+
+  
+
 </script>
